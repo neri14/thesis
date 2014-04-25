e3c7856cfea5126c47b3d0f5d18aee904fe6b83c
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2014-04-25 16:27:19 +0000

    removed unneeded comment

--- thesis/simulator/src/simulation/simulation.cpp
+++ thesis/simulator/src/simulation/simulation.cpp
@@ -323,14 +323,5 @@ bool simulation::translate_simulation_data(world::world_description_handle desc)
 	return true;
 }
 
-//TODO simulation::start
-// - start up timer - tick event (1 second) - for event to be given (second to pass) -
-//               1second timer needs to pass AND new states need to be calculated
-// - creators/destroyers should work based on tick event
-// - tick event should cause new state calculation - after actuators are finished and sensors sent their events
-
-//TODO simulation::stop
-// - simulation stop is based on stopping tick event generator or stops itself if duration is exceeded
-
 } // namespace simulation
 } // namespace simulator
